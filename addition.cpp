f9a882368ebbd37e6228fbf777725f550fbe5d70
TanvirRahman <tanvir.rahman.ewu@gmail.com>
2020-03-09 16:59:49 +0600

    addition to n implemented

--- addition.cpp
+++ addition.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
+int add(int n)
+{
+    if(!n)
+    {
+        return 0;
+    }
+
+    return n + add(n - 1);
+}
+
+
 int main()
 {
     cout << "hello world " << endl;
+
+    cout << add(5) << endl;
 }
